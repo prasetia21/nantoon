bc29bf89d982b85014a86f514d2ba4d64d748f44
prasetia21 <75926777+prasetia21@users.noreply.github.com>
2023-07-19 10:35:29 +0700

    feat: slicing signin, signup, dashboard

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,23 +1,66 @@
 import { forwardRef, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
 
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
-    const input = ref ? ref : useRef();
+const TextInput = forwardRef(
+    (
+        {
+            type = 'text',
+            name,
+            className = '',
+            value,
+            defaultValue,
+            variant = 'primary',
+            autoComplete,
+            required,
+            isFocused = false,
+            placeholder,
+            handleChange,
+            isError,
+            ...props
+        },
+        ref
+    ) => {
+        const inputRef = ref || useRef();
 
-    useEffect(() => {
-        if (isFocused) {
-            input.current.focus();
-        }
-    }, []);
-
-    return (
-        <input
-            {...props}
-            type={type}
-            className={
-                'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
-                className
+        useEffect(() => {
+            if (isFocused) {
+                inputRef.current.focus();
             }
-            ref={input}
-        />
-    );
-});
+        }, [isFocused]);
+
+        return (
+            <input
+                {...props}
+                type={type}
+                name={name}
+                value={value}
+                defaultValue={defaultValue}
+                className={`rounded-2xl bg-form-bg py-[13px] px-7 w-full ${
+                    isError ? 'input-error' : ''
+                } input-${variant} ${className}`}
+                placeholder={placeholder}
+                ref={inputRef}
+                autoComplete={autoComplete}
+                required={required}
+                onChange={(e) => handleChange(e)}
+            />
+        );
+    }
+);
+
+TextInput.propTypes = {
+    type: PropTypes.oneOf(['text', 'email', 'password', 'file']),
+    name: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+    variant: PropTypes.oneOf(['primary', 'error', 'primary-outline']),
+    autoComplete: PropTypes.string,
+    required: PropTypes.bool,
+    isFocused: PropTypes.bool,
+    handleChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    isError: PropTypes.bool
+};
+
+export default TextInput;
---
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -0,0 +1,60 @@
+import Flickity from 'react-flickity-component';
+import { Head } from '@inertiajs/react';
+import Authenticated from '@/Layouts/Authenticated/index';
+import FeaturedMovie from '@/Components/FeaturedMovie';
+import MovieCard from '@/Components/MovieCard';
+
+
+export default function Dashboard () {
+    const flickityOptions = {
+        cellAlign: "left",
+        contain: true,
+        groupCells: 1,
+        wrapAround: false,
+        pageDots: false,
+        prevNextButtons: false,
+        draggable: ">1",
+    };
+
+    return ( 
+        <Authenticated>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="/css/flickity.min.css"
+                />
+                <title>Dashboard</title>
+            </Head>
+            <div>
+                    <div className="font-semibold text-[22px] text-black mb-4">Featured Movies</div>
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                    {[1, 2, 3, 4].map((i) => (
+                        <FeaturedMovie
+                        key={i}
+                        slug="the-batman"
+                        name={`The Batman ${i}`}
+                        category="Comedy"
+                        thumbnail="/images/featured-2.png"
+                        rating={i + 1}
+                    />
+                    ))}
+                    </Flickity>
+                </div>
+                <div className="mt-[50px]">
+                    <div className="font-semibold text-[22px] text-black mb-4">Browse</div>
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                        {[1, 2, 3, 4, 5, 6].map((i) => (
+                            <MovieCard 
+                            key={i}
+                            slug="the-batman"
+                            name={`The Batman ${i}`}
+                            category="Comedy"
+                            thumbnail="/images/featured-2.png"
+                        />
+                        ))}
+                    </Flickity>
+                </div>
+        </Authenticated>
+        
+    )
+}
